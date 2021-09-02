1e055fee837e0b1b25f36a716f25d79c85b712ec
JohnnyW10Desktop <johnny.wesley@gmail.com>
2021-09-02 19:23:19 -0300

    50p

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,11 +1,20 @@
 import {Request,Response} from 'express';
 
 export const home = (req:Request,res:Response) =>{
-    res.render('pages/page')
+    res.render('pages/page',{banner:{
+        titulo:'todos os animais aqui',
+        background:'allanimals.jpg'
+    }
+})
+
 }
 
 export const dogs = (req:Request,res:Response) =>{
-    res.send('pagina caes')
+    res.render('pages/caes',{banner:{
+        titulo:'todos os caes',
+        background:'banner_dog.jpg'
+    }
+})
 }
 
 export const cats = (req:Request,res:Response) =>{
