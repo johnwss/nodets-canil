c59367a627b644d1d2d7a2686250622b9142e42b
JohnnyW10Desktop <johnny.wesley@gmail.com>
2021-09-02 14:31:30 -0300

    mvc

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,18 @@
+import {Request,Response} from 'express';
+
+export const home = (req:Request,res:Response) =>{
+    res.send('pagina home')
+}
+
+export const dogs = (req:Request,res:Response) =>{
+    res.send('pagina home')
+}
+
+export const cats = (req:Request,res:Response) =>{
+    res.send('pagina home')
+}
+
+export const fishs = (req:Request,res:Response) =>{
+    res.send('pagina home')
+}
+
